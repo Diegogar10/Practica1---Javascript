797007f24347cbb7ec6157697af7567f4dd730e5
Diego Garcia <diegogar92@gmail.com>
2021-07-01 12:30:46 -0500

    encapsulamiento de formulas en funciones

--- figuras.js
+++ figuras.js
@@ -1,53 +1,40 @@
 // Figura Cuadrado
 console.group("Cuadrados");
 
-const ladoCuadrado = 5;
-console.log("Los lados del cuadrado miden: " + ladoCuadrado + " cm.");
+let perimetroCuadrado = (lado)=>{
+    return lado * 4;
+}
 
-const perimetroCuadrado = ladoCuadrado * 4;
-console.log("El perimetro del cuadrado son: " + perimetroCuadrado + " cm.");
-
-const areaCuadrado = ladoCuadrado * ladoCuadrado;
-console.log("El area es: " + areaCuadrado + " cm^2.");
+let areaCuadrado = (lado) =>{
+    return lado * lado;
+}
 
 console.groupEnd();
 
 // Figura Triangulo
 console.group("Triangulos");
 
-const ladoTriangulo1 = 6;
-const ladoTriangulo2 = 6;
-const baseTriangulo = 4;
-console.log(`
-    Los lados del triangulo son: 
-    ${ladoTriangulo1} cm,
-    ${ladoTriangulo2} cm,
-    ${baseTriangulo} cm.
-`);
+let perimetroTriangulo = (lado1, lado2, base) => {
+    return lado1 + lado2 + base;
+}
 
-const perimetroTriangulo = ladoTriangulo1 + ladoTriangulo2 + baseTriangulo;
-console.log("El perimetro del triangulo son: " + perimetroTriangulo + " cm.");
-
-const alturaTriangulo = 5.5;
-const areaTriangulo = (baseTriangulo * alturaTriangulo) / 2;
-console.log("El area es: " + areaTriangulo + " cm^2.");
+let areaTriangulo = (base, altura) => {
+    return (base*altura)/2;
+}
 
 console.groupEnd();
 
 // Figura Circulo
 console.group("Circulos");
 
-const radioCirculo = 4;
-console.log("El radio del circulo es: " + radioCirculo + " cm.");
-
 const PI = Math.PI;
-console.log("El el valor de PI es: " + PI);
 
-const perimetroCirculo = radioCirculo * PI;
-console.log("El perimetro del circulo es: " + perimetroCirculo + " cm.");
+let perimetroCirculo = (radio) => {
+    return radio * PI;
+}
 
-const areaCirculo = (radioCirculo * radioCirculo) * PI;
-console.log("El area del circulo es: " + areaCirculo + " cm.")
-
+let areaCirculo = (radio)=>{
+    return (radio * radio) * PI;
+}
 
 console.groupEnd();
